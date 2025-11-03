6b89d87c8201080349cb184c4ea7ae41e593a057
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2025-11-02 20:17:52 -0500

    Added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,5 +9,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss()
   ],
 })
